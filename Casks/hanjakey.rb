9a6414b8efca0679fe2c07cb200467ff50d3d033
ASQi <rt.kirby2009@gmail.com>
2026-06-23 11:34:18 +0900

    fix(hanjakey): drop unnecessary verified url parameter (brew audit)

--- Casks/hanjakey.rb
+++ Casks/hanjakey.rb
@@ -2,8 +2,7 @@ cask "hanjakey" do
   version "0.1.1"
   sha256 "5642bbceb36c61ccb8ef5263352bc1ad421b2654d7e3a35404848f16d87545f3"
 
-  url "https://github.com/SJY051/HanjaKey/releases/download/v#{version}/HanjaKey-v#{version}.dmg",
-      verified: "github.com/SJY051/HanjaKey/"
+  url "https://github.com/SJY051/HanjaKey/releases/download/v#{version}/HanjaKey-v#{version}.dmg"
   name "HanjaKey"
   desc "Hangul-to-Hanja and special-symbol converter for the menu bar"
   homepage "https://github.com/SJY051/HanjaKey"
